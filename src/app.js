5a86670519c0fb3b3241b9ecedabe01ca6a9b470
venom33cm <cmyash36@gmail.com>
2020-12-29 19:20:47 +0530

    email sending feature

--- src/app.js
+++ src/app.js
@@ -87,12 +87,12 @@ app.post("/register", async (req, res) => {
                 html: "<b>thankyou for registering  </b>", // html body
             });
 
-            console.log("Message sent: %s", info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // console.log("Message sent: %s", info.messageId);
+            // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-            // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            // // Preview only available when sending through an Ethereal account
+            // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         }
 
         main().catch(console.error);
